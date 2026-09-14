18831a9fdadd0b3a1df6bcff29de8dd487932155
keisha2705 <moyokeisha8@gmail.com>
2026-09-14 14:25:30 +0200

    set up branch

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient(process.env.MONGO_URI); 
 let db;
 
 export async function connectDB() {
